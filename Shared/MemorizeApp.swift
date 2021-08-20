03ae2480b8cb5fdcfed67a2b3ec8f2ecf7f12a66
Fauzan Achmad <fauzan@nusantarabetastudio.com>
2021-08-20 10:52:48 +0700

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Fauzan Achmad on 20/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
